a68e401ff991c6424b06177f5cbeaaf20dacf10e
Kenier202 <kenier202@gmail.com>
2024-11-29 22:52:54 -0500

    feat(DbContext): create dbcontext entity framework

---
+++ IncapSys/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IncapSys.Models.Incapacidades;
+using IncapSys.Models.Rol;
+using IncapSys.Models.Usuarios;
+using Microsoft.EntityFrameworkCore;
+
+namespace IncapSys.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> config) : base (config)  { }
+
+       public DbSet<Empleados> Usuarios { get; set; }
+       public DbSet<Roles> Roles { get; set; }
+       public DbSet<DescripcionIncapacidad> Incapacidades { get; set; }
+    }
+}
