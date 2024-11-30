a6514eb96288c8a6200ee84cd198a724e9e88750
Kenier202 <kenier202@gmail.com>
2024-11-29 23:16:19 -0500

    feat(UsuarioDTO): create UsuarioDTO

---
+++ IncapSys/DTOs/UsuarioDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace IncapSys.DTOs
+{
+    public class UsuarioDTO
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public int RolId { get; set; }
+
+    }
+}
