67139d85880979f679d1d3377ee178f16dd3bde6
Kenier202 <kenier202@gmail.com>
2024-11-30 00:02:01 -0500

    feat(IUsuariosRepository): create IUsuariosRepository

---
+++ IncapSys/Repositories/IUsuariosRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace IncapSys.Repositories
+{
+    public interface IUsuariosRepository<T>
+    {
+        public Task<IEnumerable<T>> GetAllUsuarios();
+        public Task<T> GetUsuarioById(int id);
+        public Task<T> AddUsuario(T incapacidad);
+        public Task<T> DeleteUsuario(int id);
+        public Task<T> UpdateUsuario(T incapacidad);
+        public Task Save();
+    }
+}
