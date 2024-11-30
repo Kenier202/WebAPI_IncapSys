4a851552c3e0c38f6e143039b78646a8c7c8b209
Kenier202 <kenier202@gmail.com>
2024-11-29 23:15:47 -0500

    feat(IncapacidadesDTO): create IncapacidadesDTO

---
+++ IncapSys/DTOs/IncapacidadesDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IncapSys.DTOs
+{
+    public class IncapacidadesDTO
+    {
+        public int Id { get; set; }
+        public string LugarAccidente { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaSuceso { get; set; }
+    }
+}
