9982c5c782eb37de6a57b5bb65b74305f5ef1519
Kenier202 <kenier202@gmail.com>
2024-11-29 22:06:11 -0500

    feat(folders): add basics folders

---
+++ IncapSys/Models/Incapacidades/Incapacidades.cs
@@ -0,0 +1,11 @@
+﻿namespace IncapTrack.Models.Incapacidades
+{
+    public class DescripcionIncapacidad
+    {
+        public int Id { get; set; }
+        public string LugarAccidente { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaSuceso { get; set; }
+
+    }
+}
---
+++ IncapSys/Models/Usuarios/Usuarios.cs
@@ -0,0 +1,16 @@
+﻿using IncapTrack.Controllers;
+using IncapTrack.Models.Incapacidades;
+
+namespace IncapTrack.Models.Usuarios
+{
+    public class Usuarios
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public string Rol { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public DescripcionIncapacidad Incapacidad { get; set; }
+
+}
+}
