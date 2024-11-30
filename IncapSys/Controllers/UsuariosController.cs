a4f2e96c407dfd589da8efda61e5b2216304516e
Kenier202 <kenier202@gmail.com>
2024-11-30 13:11:02 -0500

    feat(UsuariosController): create controller http

--- IncapSys/Controllers/UsuariosController.cs
+++ IncapSys/Controllers/UsuariosController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IncapSys.Interfaces.Usuarios;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IncapSys.Controllers
@@ -7,5 +8,19 @@ namespace IncapSys.Controllers
     [ApiController]
     public class UsuariosController : ControllerBase
     {
+        public readonly IUsuarioService _UsuarioService;
+        public UsuariosController(IUsuarioService _usuarios) {
+            this._UsuarioService = _usuarios;
+        }
+        [HttpGet]
+        async public Task<IActionResult> GetAll() {
+            var response = await _UsuarioService.getAll();
+            if (response.IsSucces)
+            {
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+            return StatusCode(StatusCodes.Status204NoContent, response);
+
+        }
     }
 }
