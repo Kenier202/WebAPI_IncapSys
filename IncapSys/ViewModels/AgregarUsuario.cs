9c7f71b6ebb5d3024a94c5cb3111df08caf142b4
Kenier202 <kenier202@gmail.com>
2024-11-30 00:20:18 -0500

    feat(ViewModel): add view model of AgregarUsuario

---
+++ IncapSys/ViewModels/AgregarUsuario.cs
@@ -0,0 +1,21 @@
+﻿using IncapSys.Models.Incapacidades;
+using IncapSys.Models.Rol;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace IncapSys.ViewModels
+{
+    public class AgregarUsuario
+    {
+        public int Id { get; set; }
+        [MinLength(4,ErrorMessage ="Debe tener mas de 4 letras")]
+        public string Usuario { get; set; }
+        [MinLength(4, ErrorMessage = "Debe tener mas de 4 letras")]
+        public string Contraseña { get; set; }
+        [Required]
+        public DateTime FechaRegistro { get; set; }
+        public int RolId { get; set; }
+        [MinLength(2, ErrorMessage = "Debe tener mas de 2 letras")]
+        public int IncapacidadId { get; set; }
+    }
+}
