157e539866fd8d749a3d761cecc8d7d414d57455
Kenier202 <kenier202@gmail.com>
2024-11-30 00:20:07 -0500

    feat(ViewModel): add view model of AgregarIncapacidad

---
+++ IncapSys/ViewModels/AgregarIncapacidad.cs
@@ -0,0 +1,22 @@
+﻿using IncapSys.Models.Usuarios;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace IncapSys.ViewModels
+{
+    public class AgregarIncapacidad
+    {
+        public class AgregarIncapacidadViewModel
+        {
+            public int Id { get; set; }
+            [Required]
+            public string LugarAccidente { get; set; }
+            [MinLength(5, ErrorMessage = "La descripcion debe tener mas de 5 letras")]
+            public string Descripcion { get; set; }
+            [Required]
+            public DateTime FechaSuceso { get; set; }
+            public int UsuarioId { get; set; }
+
+        }
+    }
+}
