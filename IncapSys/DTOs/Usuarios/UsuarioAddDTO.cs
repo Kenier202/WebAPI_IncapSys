c5e57d70f381ca932499a64b0b8a735632b4fd80
Kenier202 <kenier202@gmail.com>
2024-11-30 00:29:14 -0500

    feat(Usuarios): add DTOs to add and update users

---
+++ IncapSys/DTOs/Usuarios/UsuarioAddDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace IncapSys.DTOs.Usuarios
+{
+    public class UsuarioAddDTO
+    {
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public int RolId { get; set; }
+    }
+}
---
+++ IncapSys/DTOs/Usuarios/UsuarioUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace IncapSys.DTOs.Usuarios
+{
+    public class UsuarioUpdateDTO
+    {
+        public int Id { get; set; }
+        public string Usuario { get; set; }
+        public string Contraseña { get; set; }
+        public int RolId { get; set; }
+    }
+}
