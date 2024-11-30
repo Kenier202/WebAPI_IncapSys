76c19fa62f696c913c06348b145dff011ff9f704
Kenier202 <kenier202@gmail.com>
2024-11-29 23:27:01 -0500

    feat(Helpers): add response database class

---
+++ IncapSys/Helpers/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace IncapSys.Helpers
+{
+    public class Response<T>
+    {
+        public required T Result { get; set; }
+        public bool IsSucces {  get; set; }
+        public required string Message { get; set; }
+    }
+}
