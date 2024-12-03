f467dfb42ff82672babb013671ecf9f063622044
Kenier202 <kenier202@gmail.com>
2024-12-03 18:31:18 -0500

    feat(IBaseInterface): Add a new type generic to return UsuarioAddDTO

--- IncapSys/Interfaces/IBaseInterface.cs
+++ IncapSys/Interfaces/IBaseInterface.cs
@@ -2,14 +2,14 @@
 
 namespace IncapSys.Interfaces
 {
-    public interface IBaseInterface<T>
+    public interface IBaseInterface<T, TDto>
     {
         //get all incapacidades
         public Task<Response<IEnumerable<T>>> getAll();
         public Task<Response<T>> GetById(int id);
         //all get one incapacidad
-        public Task<Response<T>> CreateAt(T model);
-        public Task<Response<T>> Actualizar(T model);
+        public Task<Response<T>> CreateAt(TDto model);
+        public Task<Response<T>> Actualizar(TDto model);
         public Task<Response<T>> Delete(int id);
     }
 }
