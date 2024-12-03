f62eaa48edb4bddaae04173839d7f8b45c6d7bd6
Kenier202 <kenier202@gmail.com>
2024-12-03 18:32:32 -0500

    feat(IUsuarioService): Implement new type UsuarioAddDTO to return usuario added

--- IncapSys/Interfaces/Usuarios/IUsuarioService.cs
+++ IncapSys/Interfaces/Usuarios/IUsuarioService.cs
@@ -1,9 +1,10 @@
-﻿using IncapSys.Helpers;
+﻿using IncapSys.DTOs.Usuarios;
+using IncapSys.Helpers;
 using IncapSys.Models.Usuarios;
 
 namespace IncapSys.Interfaces.Usuarios
 {
-    public interface IUsuarioService : IBaseInterface<Empleados>
+    public interface IUsuarioService : IBaseInterface<Empleados, UsuarioAddDTO>
     {
         Task<Response<bool>> ExisteUsuario(int idUsuario);
 
