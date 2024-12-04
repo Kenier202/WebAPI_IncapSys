6a1b5b017e431f31b1d85cfae210b4c2ba784cbf
Kenier202 <kenier202@gmail.com>
2024-12-03 19:26:41 -0500

    feat(ViewModel): modify validations from usuario

--- IncapSys/ViewModels/AgregarUsuario.cs
+++ IncapSys/ViewModels/AgregarUsuario.cs
@@ -5,15 +5,12 @@ namespace IncapSys.ViewModels
 {
     public class AgregarUsuario
     {
-        public int Id { get; set; }
-        [MinLength(4,ErrorMessage ="Debe tener mas de 4 letras")]
-        public string Usuario { get; set; }
+
         [MinLength(4, ErrorMessage = "Debe tener mas de 4 letras")]
+        public string Usuario { get; set; }
+        [MinLength(1, ErrorMessage = "Debe tener mas de 1 letras")]
         public string Contraseña { get; set; }
-        [Required]
-        public DateTime FechaRegistro { get; set; }
         public int RolId { get; set; }
-        [MinLength(2, ErrorMessage = "Debe tener mas de 2 letras")]
-        public int IncapacidadId { get; set; }
+
     }
 }
