e6f067070b3b3d33a4a52f07c432f225856e3bee
Kenier202 <kenier202@gmail.com>
2024-12-07 20:03:56 -0500

    feat(IRolRepository): created interface methods

--- IncapSys/Repositories/Rol/IRolRepository.cs
+++ IncapSys/Repositories/Rol/IRolRepository.cs
@@ -1,6 +1,13 @@
-﻿namespace IncapSys.Repositories.Rol
+﻿using IncapSys.Helpers;
+
+namespace IncapSys.Repositories.Rol
 {
-    public interface IRolRepository
+    public interface IRolRepository<T>
     {
+        public Task<Response<IEnumerable<T>>> GetAllRoles();
+        public Task<Response<T>> GetRolById(int id);
+        public Task<Response<T>> AddRol(T incapacidad);
+        public Task<Response<T>> DeleteRol(int id);
+        public Task<Response<T>> UpdateRol(T incapacidad);
     }
 }
