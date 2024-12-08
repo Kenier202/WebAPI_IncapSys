bf64106b8473bca051d4a2d0eb04a29fcdd43cda
Kenier202 <kenier202@gmail.com>
2024-12-07 21:53:03 -0500

    feat(RolRepositoryService): add GetAllRoles implementation

--- IncapSys/Program.cs
+++ IncapSys/Program.cs
@@ -1,12 +1,16 @@
 using IncapSys.Interfaces.Incapacidades;
+using IncapSys.Interfaces.Rol;
 using IncapSys.Interfaces.Usuarios;
 using IncapSys.Mapper.Usuarios;
 using IncapSys.Models;
 using IncapSys.Models.Incapacidades;
+using IncapSys.Models.Rol;
 using IncapSys.Models.Usuarios;
 using IncapSys.Repositories.Incapacidades;
+using IncapSys.Repositories.Rol;
 using IncapSys.Repositories.Usuarios;
 using IncapSys.Services.IncapacidadesServices;
+using IncapSys.Services.RolServices;
 using IncapSys.Services.UsuariosServices;
 using Microsoft.EntityFrameworkCore;
 
@@ -26,13 +30,18 @@ builder.Services.AddDbContext<ApplicationDbContext>(op =>
 
 //usuarios interface
 builder.Services.AddScoped<IUsuarioService, UsuariosService>();
+// usuarios repository
 builder.Services.AddScoped<IUsuariosRepository<Empleados>, UsuarioRepositoryService>();
 
 //incapacidades interface
 builder.Services.AddScoped<IIncapacidadesService, IncapacidadesServices>();
+//repository incapacidades
 builder.Services.AddScoped<IIncapacidadesRepository<DescripcionIncapacidad>, IncapacidadesRepositoryService>();
 
 //roles interface
+builder.Services.AddScoped<IRolService, RolService>();
+//repository roles
+builder.Services.AddScoped<IRolRepository<Roles>, RolRepositoryService>();
 
 
 builder.Services.AddAutoMapper(typeof(MappingProfileUsuarios)); 
--- IncapSys/Services/RolServices/RolRepositoryService.cs
+++ IncapSys/Services/RolServices/RolRepositoryService.cs
@@ -1,11 +1,60 @@
-﻿using IncapSys.Helpers;
+﻿using AutoMapper;
+using IncapSys.Helpers;
+using IncapSys.Models;
 using IncapSys.Models.Rol;
 using IncapSys.Repositories.Rol;
+using Microsoft.EntityFrameworkCore;
 
 namespace IncapSys.Services.RolServices
 {
     public class RolRepositoryService : IRolRepository<Roles>
     {
+        private readonly ApplicationDbContext _DbContext;
+        private readonly IMapper _Mapper;
+        public RolRepositoryService(ApplicationDbContext dbContext, IMapper Mapper) {
+            this._DbContext = dbContext;
+            this._Mapper = Mapper;
+        }
+
+        async public Task<Response<IEnumerable<Roles>>> GetAllRoles()
+        {
+            try
+            {
+                var roles = await _DbContext.Roles.ToListAsync();
+
+                if (!roles.Any()) return new Response<IEnumerable<Roles>> {
+                    IsSucces = false,
+                    Message = "No se han encontrado roles",
+                    Result = null
+                };
+
+                return new Response<IEnumerable<Roles>>
+                {
+                    IsSucces = true,
+                    Message = "Roles encontrados",
+                    Result = roles
+                };
+            }
+            catch (DbUpdateException dbEx)
+            { 
+                return new Response<IEnumerable<Roles>>
+                {
+                    IsSucces = false,
+                    Message = $"Database error: {dbEx.Message}",
+                    Result = null
+                };
+            }
+            catch (Exception ex) {
+                return new Response<IEnumerable<Roles>> {
+                    IsSucces = true,
+                    Message= ex.Message,
+                    Result = null
+                };
+            }
+
+        }
+
+
         public Task<Response<Roles>> AddRol(Roles incapacidad)
         {
             throw new NotImplementedException();
@@ -16,10 +65,6 @@ namespace IncapSys.Services.RolServices
             throw new NotImplementedException();
         }
 
-        public Task<Response<IEnumerable<Roles>>> GetAllRoles()
-        {
-            throw new NotImplementedException();
-        }
 
         public Task<Response<Roles>> GetRolById(int id)
         {
