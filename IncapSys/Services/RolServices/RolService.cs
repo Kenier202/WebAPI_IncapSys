f4cefc59b4096b6f19b124e581c7219af8da15b2
Kenier202 <kenier202@gmail.com>
2024-12-08 11:05:02 -0500

    feat(RolService): add implementation to getAll roles

--- IncapSys/Services/RolServices/RolService.cs
+++ IncapSys/Services/RolServices/RolService.cs
@@ -1,6 +1,72 @@
-﻿namespace IncapSys.Services.RolServices
+﻿using IncapSys.DTOs.Rol;
+using IncapSys.Helpers;
+using IncapSys.Interfaces.Rol;
+using IncapSys.Models.Rol;
+using IncapSys.Models.Usuarios;
+using IncapSys.Repositories.Rol;
+using Microsoft.AspNetCore.DataProtection.Repositories;
+
+namespace IncapSys.Services.RolServices
 {
-    public class RolService
+    public class RolService : IRolService
     {
+        private readonly IRolRepository<Roles> _RolRepositoryService;
+        public RolService(IRolRepository<Roles> rolRepositoryService) {
+            this._RolRepositoryService = rolRepositoryService;
+        }
+
+        async public Task<Response<IEnumerable<Roles>>> getAll()
+        {
+            try
+            {
+                var response = await _RolRepositoryService.GetAllRoles();
+
+                if (response == null) return new Response<IEnumerable<Roles>> {
+                    IsSucces = response.IsSucces,
+                    Message = response.Message,
+                    Result = response.Result
+                };
+
+                return new Response<IEnumerable<Roles>> { 
+                    IsSucces = response.IsSucces,
+                    Message = response.Message,
+                    Result = response.Result
+                };
+            }
+            catch (Exception ex) {
+                return new Response<IEnumerable<Roles>> {
+                    IsSucces = false,
+                    Message = ex.Message,
+                    Result = null
+                };
+            }
+        }
+
+        public Task<Response<Empleados>> Actualizar(RolUpdateDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Empleados>> CreateAt(RolAddDto model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<Roles>> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Response<bool>> ExisteRol(int idUsuario)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public Task<Response<Roles>> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
