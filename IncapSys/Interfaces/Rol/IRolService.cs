8ca7f15d4599ee322deacfdcf58d479ac97c4312
Kenier202 <kenier202@gmail.com>
2024-12-10 21:40:39 -0500

    feat(MapperRol): add mapper from RolAddDto to Roles

--- IncapSys/Interfaces/Rol/IRolService.cs
+++ IncapSys/Interfaces/Rol/IRolService.cs
@@ -9,7 +9,7 @@ namespace IncapSys.Interfaces.Rol
     public interface IRolService : IBaseInterface<Roles>
     {
         Task<Response<bool>> ExisteRol(int idUsuario);
-        Task<Response<Empleados>> CreateAt(RolAddDto model);
-        Task<Response<Empleados>> Actualizar(RolUpdateDto model);
+        Task<Response<Roles>> CreateAt(RolAddDto model);
+        Task<Response<Roles>> Actualizar(RolUpdateDto model);
     }
 }
---
+++ IncapSys/Mapper/Rol/MapperRol.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using IncapSys.DTOs.Rol;
+using IncapSys.Models.Rol;
+
+namespace IncapSys.Mapper.Rol
+{
+    public class MapperRol : Profile
+    {
+        public MapperRol() {
+
+            CreateMap<RolAddDto, Roles>();
+        }
+    }
+}
