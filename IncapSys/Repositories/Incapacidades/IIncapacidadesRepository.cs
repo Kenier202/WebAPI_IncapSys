7a289e8d0701d791c9c0d8cb50f75ae1b341d32f
kenier <kenier202@gmail.com>
2024-12-13 16:40:09 -0500

    feat(IIncapacidadesRepository): to change type returned

--- IncapSys/Repositories/Incapacidades/IIncapacidadesRepository.cs
+++ IncapSys/Repositories/Incapacidades/IIncapacidadesRepository.cs
@@ -4,7 +4,7 @@ namespace IncapSys.Repositories.Incapacidades
 {
     public interface IIncapacidadesRepository<T>
     {
-        public Task<IEnumerable<T>> GetAllIncapacidades();
+        public Task<Response<IEnumerable<T>>> GetAllIncapacidades();
         public Task<Response<T>> GetIncapacidadesById(int id);
         public Task<Response<T>> AddIncapacidad(T incapacidad);
         public Task<Response<T>> DeleteIncapacidad(int id);
