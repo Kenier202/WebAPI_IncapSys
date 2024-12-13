a3eb68e48dc2d12a7558e18709a51a3527f560a8
kenier <kenier202@gmail.com>
2024-12-13 11:22:58 -0500

    feat(MapperIncapacidad): add mapper from DescripcionIncapacidad to IncapacidadesDto

---
+++ IncapSys/Mapper/Incapacidades/MapperIncapacidad.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using IncapSys.DTOs.Incapacidades;
+using IncapSys.Helpers;
+using IncapSys.Models.Incapacidades;
+
+namespace IncapSys.Mapper.Incapacidades
+{
+    public class MapperIncapacidad : Profile
+    {
+        public MapperIncapacidad() {
+            CreateMap<Response<DescripcionIncapacidad>, IncapacidadesDto>()
+               .ForMember(dest => dest.LugarAccidente, opt => opt.MapFrom(src => src.Result.LugarAccidente))
+               .ForMember(dest => dest.Descripcion, opt => opt.MapFrom(src => src.Result.Descripcion))
+               .ForMember(dest => dest.FechaSuceso, opt => opt.MapFrom(src => src.Result.FechaSuceso))
+               .ForMember(dest => dest.UsuarioId, opt => opt.MapFrom(src => src.Result.UsuarioId));
+        }  
+    }
+}
