91c86d2a5e8170415b82678d0074a5abc13430ac
Kenier202 <kenier202@gmail.com>
2024-12-12 23:18:12 -0500

    feat(Dto): add UsuarioId propertie

--- IncapSys/DTOs/Incapacidades/IncapacidadesAddDTO.cs
+++ IncapSys/DTOs/Incapacidades/IncapacidadesAddDTO.cs
@@ -4,5 +4,6 @@
     {
         public string LugarAccidente { get; set; }
         public string Descripcion { get; set; }
+        public int UsuarioId { get; set; }
     }
 }
--- IncapSys/DTOs/Incapacidades/IncapacidadesDTO.cs
+++ IncapSys/DTOs/Incapacidades/IncapacidadesDTO.cs
@@ -2,9 +2,10 @@
 {
     public class IncapacidadesDto
     {
-        public int Id { get; set; }
         public string LugarAccidente { get; set; }
         public string Descripcion { get; set; }
         public DateTime FechaSuceso { get; set; }
+        public int UsuarioId { get; set; }
+
     }
 }
