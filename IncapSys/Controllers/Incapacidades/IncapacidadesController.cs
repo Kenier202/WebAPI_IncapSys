9fb0be5e737c44cd68ae6fb6052d5e5dd5d8ba8a
Kenier202 <kenier202@gmail.com>
2024-12-12 23:19:28 -0500

    feat(IIncapacidadesService): add implmentation to create

--- IncapSys/Controllers/Incapacidades/IncapacidadesController.cs
+++ IncapSys/Controllers/Incapacidades/IncapacidadesController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using IncapSys.DTOs.Incapacidades;
+using IncapSys.Interfaces.Incapacidades;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace IncapSys.Controllers.Incapacidades
@@ -7,5 +9,21 @@ namespace IncapSys.Controllers.Incapacidades
     [ApiController]
     public class IncapacidadesController : ControllerBase
     {
+        private readonly IIncapacidadesService _incapacidadesService;
+        public IncapacidadesController(IIncapacidadesService incapacidadesService) {
+            this._incapacidadesService = incapacidadesService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> createAt(IncapacidadesAddDto AddIncapacidad)
+        {
+            var response = await _incapacidadesService.CreateAt(AddIncapacidad);
+            if (response.IsSucces)
+            {
+                return StatusCode(StatusCodes.Status200OK, response);
+            }
+            return StatusCode(StatusCodes.Status204NoContent, response);
+
+        }
     }
 }
