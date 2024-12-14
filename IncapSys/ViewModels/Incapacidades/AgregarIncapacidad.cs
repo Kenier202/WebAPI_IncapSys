ec921c4e0ae7a4db237375777120810ad5eb2eb9
Kenier202 <kenier202@gmail.com>
2024-12-14 15:07:51 -0500

    feat (IncapacidadesViewModel): add validations to add new incapacidad

--- IncapSys/ViewModels/Incapacidades/AgregarIncapacidad.cs
+++ IncapSys/ViewModels/Incapacidades/AgregarIncapacidad.cs
@@ -1,20 +1,18 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace IncapSys.ViewModels.Usuarios
+namespace IncapSys.ViewModels.Incapacidades
 {
     public class AgregarIncapacidad
     {
         public class AgregarIncapacidadViewModel
         {
-            public int Id { get; set; }
             [Required]
             public string LugarAccidente { get; set; }
+            [Required]
             [MinLength(5, ErrorMessage = "La descripcion debe tener mas de 5 letras")]
             public string Descripcion { get; set; }
-            [Required]
-            public DateTime FechaSuceso { get; set; }
+            [Required(ErrorMessage = "UsuarioId es requerido")]
             public int UsuarioId { get; set; }
-
         }
     }
 }
