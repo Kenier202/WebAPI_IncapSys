f638fe7e3ccb2489c4e3034ffe41fa49cbe52540
Kenier202 <kenier202@gmail.com>
2024-12-14 15:16:07 -0500

    feat(UpdateIncapacidadViewModel): add new viewmodel to update incapacidades

---
+++ IncapSys/ViewModels/Incapacidades/UpdateIncapacidad.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IncapSys.ViewModels.Incapacidades
+{
+    public class UpdateIncapacidad
+    {
+        public class UpdateIncapacidadViewModel
+        {
+            [Required]
+            public int Id { get; set; }
+            [Required]
+            public string LugarAccidente { get; set; }
+            [MinLength(5, ErrorMessage = "La descripcion debe tener mas de 5 letras")]
+            public string Descripcion { get; set; }
+            [Required]
+            public string UsuarioId { get; set; }
+
+        }
+    }
+}
