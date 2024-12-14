2de5973d45a6fa381fb86b0cc15b41f10cb3b0af
Kenier202 <kenier202@gmail.com>
2024-12-13 19:50:02 -0500

    feat(IncapacidadesDto): add propierties Id and NombreUsuario

--- IncapSys/DTOs/Incapacidades/IncapacidadesDTO.cs
+++ IncapSys/DTOs/Incapacidades/IncapacidadesDTO.cs
@@ -1,11 +1,16 @@
-﻿namespace IncapSys.DTOs.Incapacidades
+﻿using IncapSys.Models.Usuarios;
+
+namespace IncapSys.DTOs.Incapacidades
 {
     public class IncapacidadesDto
     {
+        public int Id { get; set; }
         public string LugarAccidente { get; set; }
         public string Descripcion { get; set; }
         public DateTime FechaSuceso { get; set; }
         public int UsuarioId { get; set; }
+        public string NombreUsuario { get; set; }
+
 
     }
 }
