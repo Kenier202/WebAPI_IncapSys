659c2c5f199c4060dd7a3f8afc8e313383ae41ad
Kenier202 <kenier202@gmail.com>
2024-12-14 15:09:05 -0500

    feat(IncapacidadesController): use ViewModel inCreateAt

--- IncapSys/Controllers/Incapacidades/IncapacidadesController.cs
+++ IncapSys/Controllers/Incapacidades/IncapacidadesController.cs
@@ -1,7 +1,9 @@
-﻿using IncapSys.DTOs.Incapacidades;
+﻿using AutoMapper;
+using IncapSys.DTOs.Incapacidades;
 using IncapSys.Interfaces.Incapacidades;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using static IncapSys.ViewModels.Incapacidades.AgregarIncapacidad;
 
 namespace IncapSys.Controllers.Incapacidades
 {
@@ -10,18 +12,25 @@ namespace IncapSys.Controllers.Incapacidades
     public class IncapacidadesController : ControllerBase
     {
         private readonly IIncapacidadesService _incapacidadesService;
-        public IncapacidadesController(IIncapacidadesService incapacidadesService)
+        public readonly IMapper _MappingUsuarios;
+
+        public IncapacidadesController(IIncapacidadesService incapacidadesService, IMapper mappingUsuarios)
         {
             this._incapacidadesService = incapacidadesService;
+            this._MappingUsuarios = mappingUsuarios;
+
         }
 
         [HttpPost]
-        public async Task<IActionResult> CreateAt(IncapacidadesAddDto AddIncapacidad)
+        //public async Task<IActionResult> CreateAt(IncapacidadesAddDto AddIncapacidad)
+        public async Task<IActionResult> CreateAt([FromBody] AgregarIncapacidadViewModel AddIncapacidad)
         {
-            var response = await _incapacidadesService.CreateAt(AddIncapacidad);
+            var incapacidad = _MappingUsuarios.Map<IncapacidadesAddDto>(AddIncapacidad);
+            var response = await _incapacidadesService.CreateAt(incapacidad);
+
             if (response.IsSucces)
             {
-                return CreatedAtAction(nameof(GetById), new {Id = response.Result.Id}, response);
+                return CreatedAtAction(nameof(GetById), new {id = response.Result.Id}, response);
             }
             return StatusCode(StatusCodes.Status400BadRequest, response);
 
